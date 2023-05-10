b3a451d393e8e6b0e15321c0813814174045d863
Malikadevv <malikajyrgalbaeva@gmail.com>
2023-05-10 15:57:55 +0600

    newProject react movie

---
+++ src/component/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../Header/Header.scss'
+const Header = () => {
+    return (
+        <div id='header'>
+            <div className="container">
+                <div className="header">
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
